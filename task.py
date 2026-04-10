c90b3c081328405f09164b2821ca4658f490a5c8
Partho <parthomaji10@gmail.com>
2026-04-10 21:40:54 +0530

    Create task.py

---
+++ task.py
@@ -0,0 +1,29 @@
+TASKS = [
+    {
+        "id": "task1",
+        "name": "focus_task_1",
+        "grader": "grade_focus",
+        "grader_module": "grader",
+        "grader_fn": "grade_focus",
+        "description": "Easy: Study for 3 steps"
+    },
+    {
+        "id": "task2",
+        "name": "focus_task_2",
+        "grader": "grade_focus",
+        "grader_module": "grader",
+        "grader_fn": "grade_focus",
+        "description": "Medium: Balance study and rest"
+    },
+    {
+        "id": "task3",
+        "name": "focus_task_3",
+        "grader": "grade_focus",
+        "grader_module": "grader",
+        "grader_fn": "grade_focus",
+        "description": "Hard: Maximize focus score"
+    }
+]
+
+def get_tasks():
+    return TASKS
